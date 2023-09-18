6ba2f4c437b029c8c41954ec61f542875112d181
crossroads30 <crossroads30@mail.ru>
2023-09-18 20:01:52 +0300

    refactor: remove file

---
+++ src/components/sidebar-stuff/Sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+import cl from './Sidebar.module.css'
+import { NavLink } from 'react-router-dom'
+import Friends from '../Friends/Friends'
+
+const Sidebar = (props) => {
+	return (
+		<aside className={cl.sidebar}>
+			<nav className={cl.nav}>
+				<ul className={cl.list}>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? cl.active : cl.link)}
+							to='/profile'
+						>
+							Profile
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? cl.active : cl.link)}
+							to='/dialogs'
+						>
+							Messages
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? cl.active : cl.link)}
+							to='/news'
+						>
+							News
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? cl.active : cl.link)}
+							to='/music'
+						>
+							Music
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? cl.active : cl.link)}
+							to='/settings'
+						>
+							Settings
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+			<Friends data={props.data}/>
+		</aside>
+	)
+}
+
+export default Sidebar
