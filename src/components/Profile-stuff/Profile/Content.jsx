86b19e3eaa27aa7ef62ee63fe9b4d1a1c8d3fd62
crossroads30 <crossroads30@mail.ru>
2023-09-19 19:49:53 +0300

    feat: add props to posts

--- src/components/Profile-stuff/Profile/Content.jsx
+++ src/components/Profile-stuff/Profile/Content.jsx
@@ -1,15 +1,20 @@
-import Post from '../Post/Post'
 import Posts from '../Posts/Posts'
 import User from '../User/User'
 import cl from './Content.module.css'
 
-const Content = (props) => {
+const Profile = (props) => {
 	return (
 		<main className={cl.content}>
 			<User />
-			<Posts posts={props.data.posts} />
+			<Posts
+				posts={props.data.posts}
+				addPost={props.addPost}
+				newPostText={props.data.newPostText}
+				updateNewPostText={props.updateNewPostText}
+			/>
+			{/*addPost - это функция*/}
 		</main>
 	)
 }
 
-export default Content
+export default Profile
