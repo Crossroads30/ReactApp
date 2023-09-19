4b1288f998c10df4778036a7b760d709468ba7af
crossroads30 <crossroads30@mail.ru>
2023-09-19 21:31:13 +0300

    refactor: add store object

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import state, {
-	updateNewPostText,
-	updateNewMessage,
-	addMessage,
-	addPost,
-  subscribe,
-} from './redux/state'
+import store from './redux/state'
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -15,13 +9,13 @@ const rerenderTree = (state) => {
 	root.render(
 		<App
 			appData={state}
-			addPost={addPost}
-			updateNewPostText={updateNewPostText}
-			addMessage={addMessage}
-			updateNewMessage={updateNewMessage}
+			addPost={store.addPost.bind(store)}
+			updateNewPostText={store.updateNewPostText.bind(store)}
+			addMessage={store.addMessage.bind(store)}
+			updateNewMessage={store.updateNewMessage.bind(store)}
 		/>
 	)
 }
-rerenderTree(state)
+rerenderTree(store.getState())
 
-subscribe(rerenderTree)
+store.rerender(rerenderTree)
