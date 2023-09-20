47adbe80e81e110ea4336373287f7e6446b78295
crossroads30 <crossroads30@mail.ru>
2023-09-20 19:22:06 +0300

    feat: add container component

---
+++ src/components/Profile-stuff/Posts/PostsContainer.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import {
+// 	addPostActionCreator,
+// 	updateNewPostActionCreator,
+// } from '../../../redux/profile-reducer'
+// import Posts from './Posts'
+
+// const PostsContainer = props => {
+
+// 	const onAddPost = () => {
+// 		props.dispatch(addPostActionCreator())
+// 	}
+
+// 	const onPostChange = () => {
+// 		const action = props.updateNewPostText(newText)
+// 		props.dispatch(updateNewPostActionCreator(action))
+// 	}
+
+// 	return <Posts updateNewPostText={onPostChange} addPost={onAddPost} />
+// }
+
+// export default PostsContainer
