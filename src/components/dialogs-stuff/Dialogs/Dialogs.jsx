da2a12dc0108afac0b7017b189ec9bae68846f63
crossroads30 <crossroads30@mail.ru>
2023-09-20 12:48:56 +0300

    feat: add avatar

--- src/components/dialogs-stuff/Dialogs/Dialogs.jsx
+++ src/components/dialogs-stuff/Dialogs/Dialogs.jsx
@@ -2,27 +2,37 @@ import cl from './Dialogs.module.css'
 import DialogItem from '../DialogItem/DialogItem'
 import DialogMessage from '../DialogMessage/DialogMessage'
 import React from 'react'
+import {
+	addMessageActionCreator,
+	updateNewMessageActionCreator,
+} from '../../../redux/state'
 
-const Dialogs = (props) => {
+const Dialogs = props => {
 
 	const dialogItems = props.data.dialogs.map(dialog => (
-		<DialogItem name={dialog.name} id={dialog.id} key={dialog.id} />
+		<div className={cl.avatar}>
+			<div className={cl.image}></div>
+			<DialogItem name={dialog.name} id={dialog.id} key={dialog.id} />
+		</div>
 	))
+
 	const DialogMessages = props.data.messages.map(message => (
 		<DialogMessage message={message.message} id={message.id} key={message.id} />
 	))
 
-	const newDialogMessage = React.createRef()
+	// const newDialogMessage = React.createRef()
 
-
-	const onMessageTextChange = () => {
-		const newMessage = newDialogMessage.current.value
-		props.updateNewMessage(newMessage)
+	const onMessageTextChange = (event) => {
+		const message = event.target.value // вместо React.createRef() добавляем в textarea значения через event.target.value
+		// const message = newDialogMessage.current.value
+		// props.updateNewMessage(newMessage)
+		props.dispatch(updateNewMessageActionCreator(message))
 	}
 
-		const addMessage = () => {
-			props.addMessage()
-		}
+	const addMessage = () => {
+		// props.addMessage()
+		props.dispatch(addMessageActionCreator())
+	}
 
 	return (
 		<div className={cl.dialogsWrapper}>
@@ -32,7 +42,7 @@ const Dialogs = (props) => {
 			</div>
 			<textarea
 				onChange={onMessageTextChange}
-				ref={newDialogMessage}
+				// ref={newDialogMessage}
 				placeholder=' enter message'
 				value={props.data.newMessageText}
 			></textarea>
