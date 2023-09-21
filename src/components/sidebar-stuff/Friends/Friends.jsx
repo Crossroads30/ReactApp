49f6dc33442762b9758e4df8fb5f7a96018c6261
crossroads30 <crossroads30@mail.ru>
2023-09-21 16:08:38 +0300

    refactor: remove storeContext, add props

--- src/components/sidebar-stuff/Friends/Friends.jsx
+++ src/components/sidebar-stuff/Friends/Friends.jsx
@@ -1,35 +1,17 @@
 import cl from './Friends.module.css'
 import Friend from '../Friend/Friend'
-import StoreContext from '../../../context/Store-context'
 
-const Friends = () => {
+const Friends = (props) => {
 	return (
-		<StoreContext.Consumer>
-			{store => (
-				<div className={cl.friendsWrapper}>
-					<h2 className={cl.title}>Friends</h2>
-					<div className={cl.friends}>
-						{store.getState().sidebar.friends.map(friend => (
-							<Friend name={friend.name} key={friend.id} />
-						))}
-					</div>
-				</div>
-			)}
-		</StoreContext.Consumer>
+		<div className={cl.friendsWrapper}>
+			<h2 className={cl.title}>Friends</h2>
+			<div className={cl.friends}>
+				{props.friends.map(friend => (
+					<Friend name={friend.name} key={friend.id} />
+				))}
+			</div>
+		</div>
 	)
 }
 
 export default Friends
-
-// const Friends = props => {
-// 	return (
-// 		<div className={cl.friendsWrapper}>
-// 			<h2 className={cl.title}>Friends</h2>
-// 			<div className={cl.friends}>
-// 				{props.data.friends.map(friend => (
-// 					<Friend name={friend.name} key={friend.id} />
-// 				))}
-// 			</div>
-// 		</div>
-// 	)
-// }
