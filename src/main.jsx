3f0f6a97fafc0e3c01c6aca708a6e275996a9e62
crossroads30 <crossroads30@mail.ru>
2023-09-21 16:05:54 +0300

    fix: delete rerender func

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import store from './redux/redux-store' //store из redux
-import { Provider } from './context/Store-context'
+import { Provider } from 'react-redux'
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
 
-//используем контекст:
-const rerenderTree = () => {
 	root.render(
 		<Provider store={store}>
 			<App />
 		</Provider>
 	)
-}
-
-rerenderTree(store.getState())
-
-store.subscribe(() => {
-	let state = store.getState()
-	rerenderTree(state)
-}) // state(this._callRerenderTree(this._state)) // для redux-store не вызывается !!!!!
-
-
-//не используем контекст:
-// const rerenderTree = state => {
-// 	root.render(
-// 			<App state={state} dispatch={store.dispatch.bind(store)} store={store} />
-// 	)
-// }
