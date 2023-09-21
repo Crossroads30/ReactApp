757f499e6c557c1042b7eb36060f1299fea64c9f
crossroads30 <crossroads30@mail.ru>
2023-09-21 08:49:04 +0300

    refactor: replace props

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import store from './redux/redux-store/redux-store' //store из redux
-// import store from './redux/my-store/store' // мой store
+import store from './redux/redux-store' //store из redux
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
 
-const rerenderTree = (state) => {
+const rerenderTree = state => {
 	root.render(
-		<App
-			appData={state}
-			dispatch={store.dispatch.bind(store)}
-			// addPost={store.addPost.bind(store)}
-			// updateNewPostText={store.updateNewPostText.bind(store)}
-			// addMessage={store.addMessage.bind(store)}
-			// updateNewMessage={store.updateNewMessage.bind(store)}
-		/>
+		<App state={state} dispatch={store.dispatch.bind(store)} store={store} />
 	)
 }
 
 rerenderTree(store.getState())
 
 store.subscribe(() => {
-	let state = store.getState() 
+	let state = store.getState()
 	rerenderTree(state)
-}) // для redux store т.к. state(this._callRerenderTree(this._state)) // для redux-store не вызывается как в варианте ниже!!!!!
-
-
-// rerenderTree(store.getState())
-// store.subscribe(rerenderTree)// для своего store
+}) // для redux store т.к. state(this._callRerenderTree(this._state)) // для redux-store не вызывается !!!!!
