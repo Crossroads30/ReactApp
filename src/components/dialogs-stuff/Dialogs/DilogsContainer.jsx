4f22dc6907cdab0785985c20cd633d62ff08c49d
crossroads30 <crossroads30@mail.ru>
2023-09-21 16:10:40 +0300

    refactor: add connect from react-redux

--- src/components/dialogs-stuff/Dialogs/DilogsContainer.jsx
+++ src/components/dialogs-stuff/Dialogs/DilogsContainer.jsx
@@ -1,52 +1,64 @@
-import React from 'react'
 import {
 	addMessageActionCreator,
 	updateNewMessageActionCreator,
 } from '../../../redux/message-reducer'
 import Dialogs from './Dialogs'
-import StoreContext from '../../../context/Store-context'
+import { connect } from 'react-redux'
 
-const DialogsContainer = props => {
+// const DialogsContainer = props => {
 
-// 	const state = props.store.getState().messagesPage
+// // 	const state = props.store.getState().messagesPage
 
-// 	const onMessageTextChange = message => {
-// 		const action = updateNewMessageActionCreator(message)
-// 		props.store.dispatch(action)
-// 	}
+// // 	const onMessageTextChange = message => {
+// // 		const action = updateNewMessageActionCreator(message)
+// // 		props.store.dispatch(action)
+// // 	}
 
-// 	const onAddMessage = () => {
-// 		props.store.dispatch(addMessageActionCreator())
-// 	}
+// // 	const onAddMessage = () => {
+// // 		props.store.dispatch(addMessageActionCreator())
+// // 	}
 
-	return (
-		<StoreContext.Consumer>
-			{store => {
-				const state = store.getState().messagesPage
+// 	return (
+// 		<StoreContext.Consumer>
+// 			{store => {
+// 				const state = store.getState().messagesPage
 
-				const onMessageTextChange = message => {
-					const action = updateNewMessageActionCreator(message)
-					store.dispatch(action)
-				}
+// 				const onMessageTextChange = message => {
+// 					const action = updateNewMessageActionCreator(message)
+// 					store.dispatch(action)
+// 				}
 
-				const onAddMessage = () => {
-					store.dispatch(addMessageActionCreator())
-				}
+// 				const onAddMessage = () => {
+// 					store.dispatch(addMessageActionCreator())
+// 				}
 
-				return <Dialogs
-					addMessage={onAddMessage}
-					updateNewMessage={onMessageTextChange}
-					messagesPage={store}
-				/>
-			}}
-		</StoreContext.Consumer>
-	)
+// 				return <Dialogs
+// 					addMessage={onAddMessage}
+// 					updateNewMessage={onMessageTextChange}
+// 					messagesPage={store}
+// 				/>
+// 			}}
+// 		</StoreContext.Consumer>
+// 	)
+// }
+
+const setStateToProps = (state) => {
+	return {
+		messagesPage: state.messagePage
+	}
 }
 
-export default DialogsContainer
+const setDispatchTpProps = dispatch => {
+	return {
+		addMessage: () => {
+			dispatch(addMessageActionCreator())
+		},
+		updateNewMessage: message => {
+			dispatch(updateNewMessageActionCreator(message))
+		},
+	}
+}
 
-// <Dialogs
-// 	addMessage={onAddMessage}
-// 	updateNewMessage={onMessageTextChange}
-// 	messagesPage={state}
-// />
+const DialogsContainer = connect(setStateToProps,setDispatchTpProps)(Dialogs)
+
+export default DialogsContainer
