ff0d16daa7ef0b24edece8c5c0ba235ac98e3545
crossroads30 <crossroads30@mail.ru>
2023-09-21 08:49:26 +0300

    refactor: replace props

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,28 @@ import Profile from './components/Profile-stuff/Profile/Content'
 import Header from './components/Header/Header'
 import Sidebar from './components/sidebar-stuff/Sidebar/Sidebar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Dialogs from './components/dialogs-stuff/Dialogs/Dialogs'
 import Settings from './components/settings-stuff/Settings/Settings'
 import News from './components/news-stuff/News/News'
 import Music from './components/music-stuff/Music/Music'
+import DialogsContainer from './components/dialogs-stuff/Dialogs/DilogsContainer'
 
 const App = props => {
 	return (
 		<BrowserRouter>
 			<div className='app-wrapper'>
 				<Header />
-				<Sidebar data={props.appData.sidebar} />
+				<Sidebar state={props.state.sidebar} />
 				<div className='app-content'>
 					<Routes>
-						<Route
-							path='/profile'
-							element={
-								<Profile
-									data={props.appData.profilePage}
-									dispatch={props.dispatch}
-									// addPost={props.addPost}
-									// updateNewPostText={props.updateNewPostText}
-								/>
-							}
-						/>
+						<Route path='/profile' element={<Profile store={props.store} />} />
 						<Route
 							path='/dialogs/*'
-							element={
-								<Dialogs
-									data={props.appData.messagesPage}
-									dispatch={props.dispatch}
-									// updateNewMessage={props.updateNewMessage}
-									// addMessage={props.addMessage}
-								/>
-							}
+							element={<DialogsContainer store={props.store} />}
 						/>
 						<Route path='/news' element={<News />} />
 						<Route path='/music' element={<Music />} />
 						<Route path='/settings' element={<Settings />} />
-						<Route
-							path='/'
-							element={
-								<Profile
-									data={props.appData.profilePage}
-									dispatch={props.dispatch}
-									// addPost={props.addPost}
-									// updateNewPostText={props.updateNewPostText}
-								/>
-							}
-						/>
+						<Route path='/' element={<Profile store={props.store} />} />
 					</Routes>
 				</div>
 			</div>
