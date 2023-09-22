815394a90b7649e43b49fe565b91e72dbf3141e6
crossroads30 <crossroads30@mail.ru>
2023-09-22 12:30:11 +0300

    fix: rename file

--- src/react-redux/react-redux-store.js
+++ src/react-redux/react-redux-store.js
@@ -1,12 +1,14 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux'
 import profileReducer from './profile-reducer'
 import messageReducer from './message-reducer'
+import usersReducer from './users-reducer'
 import sidebarReducer from './sidebar-reducer'
 
 const reducers = combineReducers({
 	profilePage: profileReducer,
 	messagesPage: messageReducer,
 	sidebar: sidebarReducer,
+	usersPage: usersReducer,
 })
 
 const store = createStore(reducers)
