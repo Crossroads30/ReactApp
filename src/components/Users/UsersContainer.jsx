2149f2bb460bc01074558daf777f2eeb39d1f744
crossroads30 <crossroads30@mail.ru>
2023-09-27 16:22:39 +0300

    refactor: combine code in one file

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,13 +1,64 @@
+import axios from 'axios'
+import React from 'react'
+import Users from './Users'
+
 import {
-	followUserAC,
-	setUsersAC,
-	unfollowUserAC,
-	setCurrentPageAC,
+	followUser,
+	setUsers,
+	unfollowUser,
+	setCurrentPage,
+	setIsLoading,
 	// setTotalUsersCountAC
 } from '../../react-redux/users-reducer'
-import UsersApiContainer from './UsersApiContainer'
-import Users from './Users(Class)'
 import { connect } from 'react-redux'
+
+
+
+class UsersContainer extends React.Component {
+	componentDidMount() {
+		this.props.setIsLoading(true)
+		axios
+			.get(
+				`http://localhost:3000/items/?_limit=${this.props.pageSize}&_page=${this.props.currentPage}`
+			)
+			.then(response => {
+				this.props.setIsLoading(false)
+				this.props.setUsers(response.data)
+				// this.props.setTotalUsersCount(response.data.count.totalCount)
+			})
+	}
+
+	onPageChange = pageNumber => {
+		//!!!!обратить внимание что этот синтаксис этого метода - стрелочная функция!!!!
+		this.props.setCurrentPage(pageNumber)
+		this.props.setIsLoading(true)
+		axios
+			.get(
+				`http://localhost:3000/items?_limit=${this.props.pageSize}&_page=${pageNumber}`
+			)
+			.then(response => {
+				this.props.setUsers(response.data)
+				this.props.setIsLoading(false)
+			})
+	}
+
+	render() {
+		return (
+			<>
+				<Users
+					totalUsersCount={this.props.totalUsersCount}
+					pageSize={this.props.pageSize}
+					currentPage={this.props.currentPage}
+					onPageChange={this.onPageChange}
+					users={this.props.users}
+					unfollow={this.props.unfollow}
+					follow={this.props.follow}
+					isLoading={this.props.isLoading}
+				/>
+			</>
+		)
+	}
+}
 
 const setStateToProps = state => {
 	return {
@@ -15,28 +66,40 @@ const setStateToProps = state => {
 		pageSize: state.usersPage.pageSize,
 		totalUsersCount: state.usersPage.totalUsersCount,
 		currentPage: state.usersPage.currentPage,
+		isLoading: state.usersPage.isLoading,
 	}
 }
 
-const setDispatchTpProps = dispatch => {
-	return {
-		follow: userId => {
-			dispatch(followUserAC(userId))
-		},
-		unfollow: userId => {
-			dispatch(unfollowUserAC(userId))
-		},
-		setUsers: users => {
-			dispatch(setUsersAC(users))
-		},
-		setCurrentPage: pageNumber => {
-			dispatch(setCurrentPageAC(pageNumber))
-		},
-		// setTotalUsersCount: usersCount => {
-		// 	dispatch(setTotalUsersCountAC(usersCount))
-		// }
-	}
-}
+// const setDispatchTpProps = dispatch => {
+// 	return {
+// 		follow: userId => {
+// 			dispatch(followUserAC(userId))
+// 		},
+// 		unfollow: userId => {
+// 			dispatch(unfollowUserAC(userId))
+// 		},
+// 		setUsers: users => {
+// 			dispatch(setUsersAC(users))
+// 		},
+// 		setCurrentPage: pageNumber => {
+// 			dispatch(setCurrentPageAC(pageNumber))
+// 		},
+// 		setIsLoading: isLoading => {
+// 			dispatch(setIsLoadingAC(isLoading))
+// 		}
+// 		// setTotalUsersCount: usersCount => {
+// 		// 	dispatch(setTotalUsersCountAC(usersCount))
+// 		// }
+// 	}
+// }
 
-export default connect(setStateToProps, setDispatchTpProps)(UsersApiContainer)
-// export default connect(setStateToProps, setDispatchTpProps)(Users)
+//вместо setDispatchTpProps помещаем объект с ссылками на объекты в 'connect'
+export default connect(setStateToProps, {
+	followUser,
+	unfollowUser,
+	setUsers,
+	setCurrentPage,
+	setIsLoading,
+	// setTotalUsersCount
+})(UsersContainer)
+
