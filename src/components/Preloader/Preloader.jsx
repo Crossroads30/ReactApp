d0efec0171bba5da57d5038bf2040af384aad5b6
crossroads30 <crossroads30@mail.ru>
2023-09-27 16:19:44 +0300

    feat: add preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import cl from './Preloader.module.css'
+import loader from '../../assets/images/loading-gif-png-5.gif'
+
+const Preloader = () => {
+	return (
+		<div>
+			<img className={cl.loader} src={loader} />
+		</div>
+	)
+}
+
+export default Preloader
