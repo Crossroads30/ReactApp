04c12d547b5ebecbb39ecd443f1026211f910f44
crossroads30 <crossroads30@mail.ru>
2023-09-28 15:45:46 +0300

    feat: import 'withRouter' func

--- src/components/Profile-stuff/Profile/ProfileContainer.jsx
+++ src/components/Profile-stuff/Profile/ProfileContainer.jsx
@@ -3,30 +3,48 @@ import React from 'react'
 import axios from 'axios'
 import { setUserProfile } from '../../../react-redux/profile-reducer'
 import { connect } from 'react-redux'
+import { withRouter } from './HookWithRoute'
 
 class ProfileContainer extends React.Component {
 	componentDidMount() {
-		axios
-			.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+		// debugger
+		let profileId = this.props.match.params.userId
+		if (!profileId) {
+			profileId = 2
+		}
+			axios
+				.get(
+					`https://social-network.samuraijs.com/api/1.0/profile/` + profileId
+				)
 
-			.then(response => {
-				console.log(response)
-				this.props.setUserProfile(response.data)
-			})
+				.then(response => {
+					// console.log(response)
+					// console.log(profileId)
+					this.props.setUserProfile(response.data)
+				})
 	}
 
+	// getId() {
+	// 	
+	// 	return console.log(profileId)
+	// }
+
 	render() {
-		return <Profile {...this.props} userProfile={this.props.userProfile}/>
+		return <Profile {...this.props} userProfile={this.props.userProfile} />
 	}
 }
 
-// export default ProfileContainer
+
+
 const setStateToProps = state => {
 	return {
 		userProfile: state.profilePage.userProfile,
 	}
 }
 
-export default connect(setStateToProps, {
-	setUserProfile,
-})(ProfileContainer)
+const WithUrlDataContainerComponent = withRouter(ProfileContainer)
+
+// export default connect(setStateToProps, { setUserProfile })(ProfileContainer)
+export default connect(setStateToProps, { setUserProfile })(
+	WithUrlDataContainerComponent
+)
