3b993662e6f861391d6f58b5788f89383f8509f7
crossroads30 <crossroads30@mail.ru>
2023-09-28 17:32:28 +0300

    feat: add auth-reducer

--- src/react-redux/react-redux-store.js
+++ src/react-redux/react-redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from './profile-reducer'
 import messageReducer from './message-reducer'
 import usersReducer from './users-reducer'
 import sidebarReducer from './sidebar-reducer'
+import authReducer from './auth-reducer'
 
 const reducers = combineReducers({
 	profilePage: profileReducer,
 	messagesPage: messageReducer,
 	sidebar: sidebarReducer,
 	usersPage: usersReducer,
+	auth: authReducer,
 })
 
 const store = createStore(reducers)
