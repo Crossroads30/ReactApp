58c9bbc0e3d343abe71dd58b211347b6bc184862
crossroads30 <crossroads30@mail.ru>
2023-10-05 20:17:05 +0300

    feat: create form element template

---
+++ src/UI/components/common/FormsControls/FormElement.jsx
@@ -0,0 +1,17 @@
+import cl from './FormElement.module.css'
+
+export const FormElement = ({ input, meta, ...props }) => {
+	//шаблонный вариант передачи нужного элемента что бы избежать дублирования кода для различных элементов формы
+	const hasError = meta.touched && meta.error
+
+	return (
+		<div className={cl.formControl + ' ' + (hasError ? cl.error : '')}>
+			<div>
+				<props.typeofelement {...input} {...props} />
+				{/* props.typeofelement - кастомный атрибут(не camelCase т.к. реакт ругается на него) */}
+				{/* передаем в самой форме в 'Field': сам компонент -> (component={FormElement}) и нужный элемент с помощью -> (elementType='<названия элемента>') например: <Field  component={Element} elementType='input'> */}
+			</div>
+			{hasError && <span>{meta.error}</span>}
+		</div>
+	)
+}
