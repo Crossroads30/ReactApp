b1d42231d1a2dd097235c378a1e849306dfbf864
crossroads30 <crossroads30@mail.ru>
2023-10-06 14:45:58 +0300

    feat: add request to login from loginPage

--- src/UI/components/login-stuff/LoginPage/LoginPage.jsx
+++ src/UI/components/login-stuff/LoginPage/LoginPage.jsx
@@ -1,16 +1,21 @@
+import { Navigate } from 'react-router-dom'
 import { LoginReduxForm } from './LoginForm'
 import cl from './LoginPage.module.css'
 
-const LoginPage = () => {
-
-	const onSubmit = (formData) => {
+const LoginPage = props => {
+	// debugger
+	const addLogin = formData => {
 		console.log(formData)
+		props.login(formData.email, formData.password, formData.rememberMe)
 	}
 
+	if (props.isAuth) {
+		return <Navigate to='/profile' />
+	}
 	return (
 		<div className={cl.loginWrapper}>
 			<h1 className={cl.loginTitle}>Login Page</h1>
-			<LoginReduxForm onSubmit={onSubmit} />
+			<LoginReduxForm onSubmit={addLogin} />
 		</div>
 	)
 }
--- src/UI/components/login-stuff/LoginPage/LoginPageContainer.jsx
+++ src/UI/components/login-stuff/LoginPage/LoginPageContainer.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import LoginPage from './LoginPage'
+import {loginToServer} from '../../../../BLL/react-redux/auth-reducer'
 
 class LoginPageContainer extends React.Component {
 	componentDidMount() {}
 
 	render() {
-		return <LoginPage />
+		return <LoginPage login={this.props.loginToServer} isAuth={this.props.isAuth}/>
 	}
 }
 
-const setStateToProps = state => {
-	return {}
+const setStateToProps = state => { 
+	return {
+		isAuth: state.auth.isAuth
+	} 
 }
 
-export default connect(setStateToProps, {})(LoginPageContainer)
+export default connect(setStateToProps, { loginToServer })(LoginPageContainer)
+
+// export default connect(null, {loginToServer})(LoginPageContainer) //если  из state ничего не передаём, поэтому в connect пишем null
