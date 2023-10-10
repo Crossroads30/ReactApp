f221e4a85c08043aa2c71e8f358b07ed413c9cd5
crossroads30 <crossroads30@mail.ru>
2023-10-10 13:06:14 +0300

    feat: add examples of reselect library

--- src/BLL/react-redux/selectors/users-selectors.js
+++ src/BLL/react-redux/selectors/users-selectors.js
@@ -1,6 +1,28 @@
+import { createSelector } from 'reselect'
+
 export const getAllUsers = state => {
+	//простой селектор без библиотеки reselect
 	return state.usersPage.users
 }
+//--------------в качестве примера-----------------------------------------------
+// export const getAllUsersSelector = state => {
+// 	//сложный( который производит дополнительные действия со state методом filter) селектор использует простой селектор(getAllUsers) без библиотеки reselect
+// 	return getAllUsers(state).filter(u => true)
+// }
+
+// export const getAllUsersSuperSelector1 = createSelector(getAllUsers, users => {
+// 	//сложный селектор созданный при помощи библиотеки reselect - берет в качестве первого параметра простой селектор чтобы получить из него значения(передаются вторым параметром) и использовать их производя с ними какие-либо действия внутри
+// 	return users.filter(u => true)
+// })
+// export const getAllUsersSuperSelector2 = createSelector(
+// 	getAllUsers,
+// 	getIsLoading,
+// 	(users, isLoading) => {
+// 		//сложный селектор созданный при помощи библиотеки reselect может принимать сразу несколько простых селекторов чтобы получить из них значения(передаются после самих селекторов) и использовать их производя с ними какие-либо действия внутри
+// 		return users.filter(u => true)
+// 	}
+// )
+//------------------------------------------------------------------------------
 export const getPageSize = state => {
 	return state.usersPage.pageSize
 }
