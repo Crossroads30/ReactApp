5e39f31ce8ea674f404f53c019af26bd4ab05197
crossroads30 <crossroads30@mail.ru>
2023-10-11 20:46:57 +0300

    fix: change key

--- src/DAL/api/api.js
+++ src/DAL/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
 	withCredentials: true,
 	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
 	headers: {
-		'API-KEY': '85844ce9-cab3-4602-9bd3-a4e1dc41aebc',
+		'API-KEY': '614d93d1-da90-4c3e-bd9c-0c093271d9f2',
 	},
 })
 
