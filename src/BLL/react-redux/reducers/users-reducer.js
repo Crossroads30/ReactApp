8531410646b8a6ccc53447b785531b711fd0112a
crossroads30 <crossroads30@mail.ru>
2023-10-12 15:12:42 +0300

    refactor use async await & temp func for mapping

--- src/BLL/react-redux/reducers/users-reducer.js
+++ src/BLL/react-redux/reducers/users-reducer.js
@@ -1,4 +1,5 @@
 import { userApi } from '../../../DAL/api/api'
+import { updateObjectInArray } from '../../../utils/helpers/object-helpers'
 
 //названия для action creators должны быть уникальными, поэтому можно добавить впереди названия самого редьюсера
 const SET_USERS = 'users/SET-USERS'
@@ -7,7 +8,8 @@ const UNFOLLOW = 'users/UNFOLLOW'
 const SET_CURRENT_PAGE = 'users/SET-CURRENT-PAGE'
 const TOGGLE_IS_LOADING = 'users/TOGGLE-IS-LOADING'
 const SET_USERS_COUNT = 'users/SET-USERS-COUNT'
-const DISABLE_BUTTON_WHILE_FOLLOWING_IN_PROGRESS = 'users/DISABLE-FETCHING-BUTTON'
+const DISABLE_BUTTON_WHILE_FOLLOWING_IN_PROGRESS =
+	'users/DISABLE-FETCHING-BUTTON'
 
 let initialState = {
 	users: [],
@@ -28,24 +30,36 @@ const UsersReducer = (state = initialState, action) => {
 		case FOLLOW:
 			return {
 				...state,
-				// users: [...state.users], //если внутри массива не надо что либо менять
-				users: state.users.map(user => {
-					if (user.id === action.userId) {
-						return { ...user, followed: true }
-					}
-					return user
-				}), //если внутри массива надо изменить какие-либо объекты
+				//если внутри массива не надо что либо менять:
+				// users: [...state.users],
+				//если внутри массива надо изменить какие-либо объекты:
+				//вместо закомментированного кода ниже импортируем шаблонную функцию helper и передаем в нее все нужные параметры:
+				users: updateObjectInArray(state.users, action.userId, 'id', {
+					followed: true,
+				}),
+				// users: state.users.map(user => {
+				// 	if (user.id === action.userId) {
+				// 		return { ...user, followed: true }
+				// 	}
+				// 	return user
+				// }),
 			}
 		case UNFOLLOW:
 			return {
 				...state,
-				// users: [...state.users], //если внутри массива не надо что либо менять
-				users: state.users.map(user => {
-					if (user.id === action.userId) {
-						return { ...user, followed: false }
-					}
-					return user
-				}), //если внутри массива надо изменить какие-либо объекты
+				//если внутри массива не надо что либо менять:
+				// // users: [...state.users],
+				//если внутри массива надо изменить какие-либо объекты:
+				//вместо закомментированного кода ниже импортируем шаблонную функцию helper и передаем в нее все нужные параметры:
+				users: updateObjectInArray(state.users, action.userId, 'id', {
+					followed: false,
+				}),
+				// users: state.users.map(user => {
+				// 	if (user.id === action.userId) {
+				// 		return { ...user, followed: false }
+				// 	}
+				// 	return user
+				// }),
 			}
 		case SET_CURRENT_PAGE:
 			return {
@@ -99,40 +113,68 @@ export const setDisableFetchingButton = (isLoading, userId) => ({
 //thunk функции:
 
 //ThunkCreator getUsers:
-export const getUsers = (currentPage, pageSize) => {
+export const getUsers = (currentPage, pageSize) => async dispatch => {
 	//берем параметры с помощью замыкания в thunkCreator и возвращаем из нее thunk функцию с диспатчем, в которую приходят эти параметры:
-	return dispatch => {
-		dispatch(setIsLoading(true)) //диспатчем actionCreator
+	dispatch(setIsLoading(true)) //диспатчем actionCreator
+	const data = await userApi.getUsers(currentPage, pageSize)
+	// data - то что пришло из ajax-запроса в DAL/api/api.js
+	dispatch(setCurrentPage(currentPage)) //диспатчем actionCreator
+	dispatch(setIsLoading(false)) //диспатчем actionCreator
+	dispatch(setUsers(data)) //диспатчем actionCreator
+}
 
-		userApi.getUsers(currentPage, pageSize).then(data => {
-			// data - то что пришло из ajax-запроса в DAL/api/api.js
-			dispatch(setCurrentPage(currentPage)) //диспатчем actionCreator
-			dispatch(setIsLoading(false)) //диспатчем actionCreator
-			dispatch(setUsers(data)) //диспатчем actionCreator
-		})
-	}
+//общая функция для follow/unfollow
+const followUnfollowFlow = async (
+	//код с одной общей функцией для follow/unfollow для избежания дублирования кода
+	dispatch,
+	userId,
+	apiMethod,
+	actionCreator
+) => {
+	dispatch(setDisableFetchingButton(true, userId))
+	let data = await apiMethod(userId)
+
+	data.resultCode === 0 && dispatch(actionCreator(userId))
+	dispatch(setDisableFetchingButton(false, userId))
 }
 
 //ThunkCreator follow:
-export const follow = userId => {
-	return dispatch => {
-		dispatch(setDisableFetchingButton(true, userId)) // передаем id пользователя кнопку которого надо сделать неактивной во время запроса на подписку
-		userApi.getFollow(userId).then(data => {
-			data.resultCode === 0 && dispatch(followUser(userId))
-			dispatch(setDisableFetchingButton(false, userId)) // // передаем id пользователя кнопку которого надо вернуть в активное состояние после того как запрос на подписку закончится
-		})
-	}
+export const follow = userId => async dispatch => {
+	followUnfollowFlow(
+		//с помощью диструктуризации передаем параметры в followUnfollowFlow
+		dispatch,
+		userId,
+		userApi.getFollow.bind(userId),
+		followUser
+	)
 }
 
 //ThunkCreator unfollow:
-export const unfollow = userId => {
-	return dispatch => {
-		dispatch(setDisableFetchingButton(true, userId)) // передаем id пользователя кнопку которого надо сделать неактивной во время запроса на отписку
-		userApi.getUnFollow(userId).then(data => {
-			data.resultCode === 0 && dispatch(unfollowUser(userId))
-			dispatch(setDisableFetchingButton(false, userId)) // // передаем id пользователя кнопку которого надо вернуть в активное состояние после того как запрос на отписку закончится
-		})
-	}
+export const unfollow = userId => async dispatch => {
+	followUnfollowFlow(
+		//с помощью диструктуризации передаем параметры в followUnfollowFlow
+		dispatch,
+		userId,
+		userApi.getUnFollow.bind(userId),
+		unfollowUser
+	)
 }
+
+//код для follow/unfollow без общей функции
+// //ThunkCreator follow:
+// export const follow = userId => async dispatch => {
+// 	dispatch(setDisableFetchingButton(true, userId)) // передаем id пользователя кнопку которого надо сделать неактивной во время запроса на подписку
+// 	const data = await userApi.getFollow(userId)
+// 	data.resultCode === 0 && dispatch(followUser(userId))
+// 	dispatch(setDisableFetchingButton(false, userId)) // // передаем id пользователя кнопку которого надо вернуть в активное состояние после того как запрос на подписку закончится
+// }
+
+// //ThunkCreator unfollow:
+// export const unfollow = userId => async dispatch => {
+// 	dispatch(setDisableFetchingButton(true, userId)) // передаем id пользователя кнопку которого надо сделать неактивной во время запроса на отписку
+// 	const data = userApi.getUnFollow(userId)
+// 	data.resultCode === 0 && dispatch(unfollowUser(userId))
+// 	dispatch(setDisableFetchingButton(false, userId)) // // передаем id пользователя кнопку которого надо вернуть в активное состояние после того как запрос на отписку закончится
+// }
 
 export default UsersReducer
