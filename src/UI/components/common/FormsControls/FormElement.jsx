c863188f799a97cd871ccb5e9c1b6c99dc1a16d2
crossroads30 <crossroads30@mail.ru>
2023-10-13 08:53:02 +0300

    refactor: destructure

--- src/UI/components/common/FormsControls/FormElement.jsx
+++ src/UI/components/common/FormsControls/FormElement.jsx
@@ -1,9 +1,12 @@
 import { Field } from 'redux-form'
 import cl from './FormElement.module.css'
 
-export const FormElement = ({ input, meta, ...props }) => {
+// export const FormElement = ({ input, meta, ...props }) => {
+export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
+	//используем диструктуризацию для meta.touched и meta.error
 	//шаблонный вариант передачи нужного элемента что бы избежать дублирования кода для различных элементов формы
-	const hasError = meta.touched && meta.error
+	// const hasError = meta.touched && meta.error
+	const hasError = touched && error //используем диструктуризацию для meta.touched и meta.error
 
 	return (
 		<div className={cl.formControl + ' ' + (hasError ? cl.error : '')}>
@@ -12,7 +15,9 @@ export const FormElement = ({ input, meta, ...props }) => {
 				{/* props.typeofelement - кастомный атрибут(не camelCase т.к. реакт ругается на него) */}
 				{/* передаем в самой форме в 'Field': сам компонент -> (component={FormElement}) и нужный элемент с помощью -> (elementType='<названия элемента>') например: <Field  component={Element} elementType='input'> */}
 			</div>
-			{hasError && <span>{meta.error}</span>}
+			{/* {hasError && <span>{meta.error}</span>} */}
+			{hasError && <span>{error}</span>}
+			{/* //используем диструктуризацию для meta.error */}
 		</div>
 	)
 }
