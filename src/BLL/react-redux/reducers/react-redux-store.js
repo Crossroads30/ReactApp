75ee6421b7a50ee8af3b5a6ccc2543ec70a9cbc6
crossroads30 <crossroads30@mail.ru>
2023-10-13 15:27:10 +0300

    feat: add redux dev-tool

--- src/BLL/react-redux/reducers/react-redux-store.js
+++ src/BLL/react-redux/reducers/react-redux-store.js
@@ -7,6 +7,7 @@ import authReducer from './auth-reducer'
 import thunkMiddleware  from 'redux-thunk'
 import { reducer as formReducer } from 'redux-form'
 import appReducer from './app-reducer'
+ import { compose } from 'redux';
 
 const reducers = combineReducers({
 	profilePage: profileReducer,
@@ -18,8 +19,14 @@ const reducers = combineReducers({
 	form: formReducer,
 })
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+ 
+//для подключения redux dev-tool:
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))//передаем в функцию createStore -> функцию composeEnhancers для подключения Chrome расширения 'redux dev-tool' в которую передаем applyMiddleware(thunkMiddleware)
 
-window.store = store
+//без подключения redux dev-tool:
+// const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+
+window.__store__ = store // обращение к глобальному store
 
 export default store
