54620af6312e8cf9f2bcf8b1ff276ea2bfa07277
crossroads30 <crossroads30@mail.ru>
2023-10-16 16:53:23 +0300

    feat: add lazy loading with suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './styles/App.css'
-import ProfileContainer from './UI/components/Profile-stuff/Profile/ProfileContainer'
+// import ProfileContainer from './UI/components/Profile-stuff/Profile/ProfileContainer'
 import Sidebar from './UI/components/sidebar-stuff/Sidebar/Sidebar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Settings from './UI/components/settings-stuff/Settings/Settings'
 import News from './UI/components/news-stuff/News/News'
 import Music from './UI/components/music-stuff/Music/Music'
-import DialogsContainer from './UI/components/dialogs-stuff/Dialogs/DialogsContainer'
+// import DialogsContainer from './UI/components/dialogs-stuff/Dialogs/DialogsContainer'
 import UsersContainer from './UI/components/Users/UsersContainer'
 import HeaderContainer from './UI/components/Header/HeaderContainer'
 import LoginPageContainer from './UI/components/login-stuff/LoginPage/LoginPageContainer'
@@ -15,6 +15,16 @@ import { initializeApp } from './BLL/react-redux/reducers/app-reducer'
 import Preloader from './UI/components/common/Preloader/Preloader'
 // import { compose } from 'redux'
 // import { withRouter } from './UI/components/Profile-stuff/Profile/HookWithRoute'
+
+//так называемая ленивая загрузка, когда компонент подгружается по необходимости
+import { lazy } from 'react'
+const DialogsContainer = lazy(() => import('./UI/components/dialogs-stuff/Dialogs/DialogsContainer'))
+const ProfileContainer = lazy(() => import('./UI/components/Profile-stuff/Profile/ProfileContainer'))
+
+// с ленивой загрузкой c помощью HOC: 
+import { withSuspense } from './HOC/withSuspense'
+const DialogsContainerWithSuspense = withSuspense(DialogsContainer)
+const ProfileContainerWithSuspense = withSuspense(ProfileContainer)
 
 class App extends React.Component {
 
@@ -28,23 +38,57 @@ class App extends React.Component {
 			return <Preloader />
 		}
 		return (
-				<div className='app-wrapper'>
-					<HeaderContainer />
-					<Sidebar />
-					<div className='app-content'>
-						<Routes>
-							<Route path='/profile/:userId?' element={<ProfileContainer />} />
-							{/* чтобы отображался 'profile' вне зависимости от URL звездочку добавляем в конец */}
-							<Route path='/dialogs/*' element={<DialogsContainer />} />
-							<Route path='/news' element={<News />} />
-							<Route path='/users' element={<UsersContainer />} />
-							<Route path='/music' element={<Music />} />
-							<Route path='/settings' element={<Settings />} />
-							<Route path='*' element={<ProfileContainer />} />
-							<Route path='/login' element={<LoginPageContainer />} />
-						</Routes>
-					</div>
+			<div className='app-wrapper'>
+				<HeaderContainer />
+				<Sidebar />
+				<div className='app-content'>
+					{/* Чтобы выполнялась так называемая ленивая загрузка страницы, нужно нужно в это время показывать загрузчик (любой div или компоненту). Чтобы отобразить загрузчик, нужно передать его в fallback в параметр компоненты React.Suspense, которой нужно обернуть импортированную компоненту. */}
+					<Routes>
+						{/* без ленивой загрузки: */}
+						{/* <Route path='/profile/:userId?' element={<ProfileContainer />} /> */}
+						{/* чтобы отображался 'profile' вне зависимости от URL звездочку добавляем в конец */}
+						{/* с ленивой загрузкой без HOC: */}
+						{/* <Route
+							path='/profile/:userId?'
+							element={
+								<React.Suspense fallback={<Preloader />}>
+									<ProfileContainer />
+								</React.Suspense>
+							}
+						/> */}
+						{/* с ленивой загрузкой c помощью HOC: */}
+						<Route path='/profile/:userId?' element={<ProfileContainerWithSuspense />} />
+						
+						{/* без ленивой загрузки: */}
+						{/* <Route path='/dialogs/*' element={<DialogsContainer />} /> */}
+						{/* с ленивой загрузкой без HOC: */}
+						{/* <Route
+							path='/dialogs/*'
+							element={
+								<React.Suspense fallback={<Preloader />}>
+									<DialogsContainer />
+								</React.Suspense>
+							}
+						/> */}
+						{/* с ленивой загрузкой c помощью HOC: */}
+						<Route path='/dialogs/*' element={<DialogsContainerWithSuspense />} />
+						<Route path='/news' element={<News />} />
+						<Route path='/users' element={<UsersContainer />} />
+						<Route path='/music' element={<Music />} />
+						<Route path='/settings' element={<Settings />} />
+						{/* <Route path='*' element={<ProfileContainer />} /> */}
+						<Route
+							path='*'
+							element={
+								<React.Suspense fallback={<Preloader />}>
+									<ProfileContainer />
+								</React.Suspense>
+							}
+						/>
+						<Route path='/login' element={<LoginPageContainer />} />
+					</Routes>
 				</div>
+			</div>
 		)
 	}
 }
