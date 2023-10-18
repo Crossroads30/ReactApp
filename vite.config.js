cba2c44d30e698eec7dd7533c36166f8d6939b4a
crossroads30 <crossroads30@mail.ru>
2023-10-18 14:17:46 +0300

    feat: add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './', // для того что бы url файлов css и js при билде прописывался корректно то корневой папки
 	plugins: [react()],
 	server: {
 		host: 'localhost',
