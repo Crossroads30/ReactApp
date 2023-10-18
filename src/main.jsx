dbb5376bfd6807175d16e70156ea5d04bd47f7a3
crossroads30 <crossroads30@mail.ru>
2023-10-18 14:16:36 +0300

    refactor: change browserRouter to hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import store from './BLL/react-redux/reducers/react-redux-store.js' //store из react-redux
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
 
 	root.render(
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter >
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	)
