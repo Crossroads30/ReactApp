18939d1eb818eec82ffe589079cb87de600ae738
crossroads30 <crossroads30@mail.ru>
2023-10-18 14:37:04 +0300

    feat: add comment

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
-
+// используем HashRouter вместо BrowserRouter, для того что бы на gh-pages браузер адекватно отображал страницы от корневой папки( названия репозитория ), для полноценных хостингов надо использовать BrowserRouter
 	root.render(
-		<HashRouter >
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
