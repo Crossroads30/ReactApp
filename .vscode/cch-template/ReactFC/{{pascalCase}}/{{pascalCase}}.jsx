1f9a68458a11f2d877c05b24a218c769cc495472
crossroads30 <crossroads30@mail.ru>
2023-11-12 21:48:36 +0300

    feat: add file

---
+++ .vscode/cch-template/ReactFC/{{pascalCase}}/{{pascalCase}}.jsx
@@ -0,0 +1,11 @@
+import cl from './{{pascalCase}}.module.css'
+
+const {{pascalCase}} = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default {{pascalCase}}
---
+++ .vscode/cch-template/ReactFC/{{pascalCase}}/{{pascalCase}}Container.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+class {{pascalCase}}Container extends React.Component {
+	componentDidMount() {
+    
+  }
+
+	render() { 
+return 
+ }
+}
+
+const setStateToProps = state => {
+	return {
+
+	}
+}
+
+export default connect(setStateToProps, {
+})({{pascalCase}}Container)
