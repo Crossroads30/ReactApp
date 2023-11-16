286714e8c96ce56d56ef130b3442af13f8c10142
crossroads30 <crossroads30@mail.ru>
2023-11-16 10:27:52 +0300

    feat: add types to validator

---
+++ A-react-typescript/src/utils/validators/validators.ts
@@ -0,0 +1,11 @@
+export type FieldValidatorType = (value: string) => string | undefined 
+
+export const required: FieldValidatorType = value => {
+	return value ? undefined : 'Field is required!'
+}
+
+export const maxLengthCreator =
+	(maxLength: number): FieldValidatorType =>
+	value => {
+		return value && value.length > maxLength ? `Max length is ${maxLength} symbols!` : undefined
+	}
