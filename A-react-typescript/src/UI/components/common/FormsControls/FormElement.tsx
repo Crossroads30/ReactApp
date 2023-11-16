8cc8e2d58399729a193516d2ba36219b0efa35c8
crossroads30 <crossroads30@mail.ru>
2023-11-16 16:49:59 +0300

    feat: add types for FormElement

--- A-react-typescript/src/UI/components/common/FormsControls/FormElement.tsx
+++ A-react-typescript/src/UI/components/common/FormsControls/FormElement.tsx
@@ -3,8 +3,19 @@ import { Field } from 'redux-form'
 import cl from './FormElement.module.css'
 import { FieldValidatorType } from '../../../../utils/validators/validators'
 
+type FormControlParamsType = {
+	input: React.FormEvent<HTMLInputElement>
+	meta: {
+		touched: boolean
+		error: string
+	}
+	typeofelement: any
+}
+
+type FormControlType = (params:FormControlParamsType) => React.ReactNode
+
 // export const FormElement = ({ input, meta, ...props }) => {
-export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
+export const FormElement: FormControlType = ({ input, meta: { touched, error }, ...props }) => {
 	//используем диструктуризацию для meta.touched и meta.error
 	//шаблонный вариант передачи нужного элемента что бы избежать дублирования кода для различных элементов формы
 	// const hasError = meta.touched && meta.error
@@ -15,7 +26,7 @@ export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
 			<div>
 				<props.typeofelement {...input} {...props} />
 				{/* props.typeofelement - кастомный атрибут(не camelCase т.к. реакт ругается на него) */}
-				{/* передаем в самой форме в 'Field': сам компонент -> (component={FormElement}) и нужный элемент с помощью -> (typeofelement='<названия элемента>') например: <Field  component={Element} typeofelement='input'> */}
+				{/* передаем в самой форме в 'Field': сам компонент -> (component={FormElement}) и нужный элемент с помощью -> (typeofelement='<названия элемента>') например: <Field  component={FormElement} typeofelement='input'> */}
 			</div>
 			{/* {hasError && <span>{meta.error}</span>} */}
 			{hasError && <span>{error}</span>}
@@ -24,7 +35,7 @@ export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
 	)
 }
 
-export const createField = (className: string | undefined, placeholder: string | undefined, name: string | undefined, validate: Array<FieldValidatorType> | null, typeofelement, props = {}, text = '') => {
+export const createField = (className: string | undefined, placeholder: string | undefined, name: string | undefined, validate: Array<FieldValidatorType> | null, typeofelement: string, props = {}, text = '') => {
 	return (
 		<div className={className}>
 			<Field placeholder={placeholder} name={name} validate={validate} component={FormElement} typeofelement={typeofelement} {...props} />
