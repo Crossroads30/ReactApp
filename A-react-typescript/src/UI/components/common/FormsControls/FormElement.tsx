1f0dc0e897d92bc7f6995f283217a18744e09ba5
crossroads30 <crossroads30@mail.ru>
2023-11-16 10:29:56 +0300

    refactor: add types to form creator

---
+++ A-react-typescript/src/UI/components/common/FormsControls/FormElement.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Field } from 'redux-form'
+import cl from './FormElement.module.css'
+import { FieldValidatorType } from '../../../../utils/validators/validators'
+
+// export const FormElement = ({ input, meta, ...props }) => {
+export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
+	//используем диструктуризацию для meta.touched и meta.error
+	//шаблонный вариант передачи нужного элемента что бы избежать дублирования кода для различных элементов формы
+	// const hasError = meta.touched && meta.error
+	const hasError = touched && error //используем диструктуризацию для meta.touched и meta.error
+
+	return (
+		<div className={cl.formControl + ' ' + (hasError ? cl.error : '')}>
+			<div>
+				<props.typeofelement {...input} {...props} />
+				{/* props.typeofelement - кастомный атрибут(не camelCase т.к. реакт ругается на него) */}
+				{/* передаем в самой форме в 'Field': сам компонент -> (component={FormElement}) и нужный элемент с помощью -> (typeofelement='<названия элемента>') например: <Field  component={Element} typeofelement='input'> */}
+			</div>
+			{/* {hasError && <span>{meta.error}</span>} */}
+			{hasError && <span>{error}</span>}
+			{/* //используем диструктуризацию для meta.error */}
+		</div>
+	)
+}
+
+export const createField = (className: string | undefined, placeholder: string | undefined, name: string | undefined, validate: Array<FieldValidatorType> | null, typeofelement, props = {}, text = '') => {
+	return (
+		<div className={className}>
+			<Field placeholder={placeholder} name={name} validate={validate} component={FormElement} typeofelement={typeofelement} {...props} />
+			{text}
+		</div>
+	)
+}
