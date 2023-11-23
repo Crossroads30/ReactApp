e620dbba2751a4e309e200d60908da2ec04997b3
crossroads30 <crossroads30@mail.ru>
2023-11-23 15:12:32 +0300

    refactor: add InferActionsTypes generic

--- A-react-typescript/src/BLL/react-redux/reducers/profile-reducer.ts
+++ A-react-typescript/src/BLL/react-redux/reducers/profile-reducer.ts
@@ -1,18 +1,19 @@
 import { ResultCodesEnum } from '../../../DAL/api/api.ts'
 import { profileApi } from '../../../DAL/api/profileApi.ts'
-import { stopSubmit } from 'redux-form'
+import { FormAction, stopSubmit } from 'redux-form'
 import { PhotosType, PostsType, ProfileType } from '../../../types/types'
 import { ThunkAction } from 'redux-thunk'
-import { AppStateType, BaseThunkType } from './react-redux-store'
+import { AppStateType, BaseThunkType, InferActionsTypes } from './react-redux-store'
 
 //названия для action creators должны быть уникальными, поэтому можно добавить впереди названия самого редьюсера
-const ADD_POST = 'profile/ADD-POST'
-const SET_USER_PROFILE = 'profile/SET-USER-PROFILE'
-const SET_USER_STATUS = 'profile/SET-USER-STATUS'
-const UPDATE_USER_STATUS = 'profile/UPDATE-USER-STATUS'
-const DELETE_POST = 'profile/DELETE-POST'
-const SAVE_USER_PHOTO_SUCCESS = 'SET-USER-PHOTO-SUCCESS'
-const UPDATE_USER_DATA_SUCCESS = 'profile/UPDATE-USER-DATA-SUCCESS'
+//!!! так как типизация(ActionTypes) не позволит записать в типы ничего другого кроме тех типов которые указаны в AC, то эти константы с названиями типов можно убрать !!!
+// const ADD_POST = 'profile/ADD-POST'
+// const SET_USER_PROFILE = 'profile/SET-USER-PROFILE'
+// const SET_USER_STATUS = 'profile/SET-USER-STATUS'
+// const UPDATE_USER_STATUS = 'profile/UPDATE-USER-STATUS'
+// const DELETE_POST = 'profile/DELETE-POST'
+// const SAVE_USER_PHOTO_SUCCESS = 'SET-USER-PHOTO-SUCCESS'
+// const UPDATE_USER_DATA_SUCCESS = 'profile/UPDATE-USER-DATA-SUCCESS'
 
 let initialState = {
 	posts: [
@@ -31,7 +32,7 @@ export type InitialStateType = typeof initialState
 
 const profileReducer = (state = initialState, action: ActionTypes): InitialStateType => {
 	switch (action.type) {
-		case ADD_POST:
+		case 'profile/ADD_POST':
 			let newPost = {
 				id: 7,
 				message: action.newPostBody,
@@ -41,32 +42,32 @@ const profileReducer = (state = initialState, action: ActionTypes): InitialState
 				...state,
 				posts: [...state.posts, newPost],
 			}
-		case DELETE_POST:
+		case 'profile/DELETE_POST':
 			return {
 				...state,
 				posts: state.posts.filter(post => post.id != action.postId),
 			}
-		case SET_USER_PROFILE:
+		case 'profile/SET_USER_PROFILE':
 			return {
 				...state,
 				userProfile: action.userProfile,
 			}
-		case SET_USER_STATUS:
+		case 'profile/SET_USER_STATUS':
 			return {
 				...state,
 				status: action.status,
 			}
-		case UPDATE_USER_STATUS:
+		case 'profile/UPDATE_USER_STATUS':
 			return {
 				...state,
 				status: action.status,
 			}
-		case SAVE_USER_PHOTO_SUCCESS:
+		case'profile/SAVE_USER_PHOTO_SUCCESS':
 			return {
 				...state,
 				userProfile: { ...state.userProfile, photos: action.userPhoto } as ProfileType, //фотография приходит из api запроса и находится в photos
 			}
-		case UPDATE_USER_DATA_SUCCESS:
+		case 'profile/UPDATE_USER_DATA_SUCCESS':
 			return {
 				...state,
 				userDataStatus: action.userDataStatus,
@@ -78,102 +79,79 @@ const profileReducer = (state = initialState, action: ActionTypes): InitialState
 
 //-------------------------------------------------------------
 
-type ActionTypes = AddPostType | DeletePostType | SetUserProfileType | SetUserStatusType | UpdateUserStatusType |SavePhotoSuccessType | UpdateUserDataSuccessType
+type ActionTypes = InferActionsTypes<typeof actions>
 
-//Action Creators with types:
-type AddPostType = {
-	type: typeof ADD_POST
-	newPostBody: string 
+const actions = {
+	addPost: (newPostBody: string) =>
+		({
+			type: 'profile/ADD_POST',
+			newPostBody,
+		} as const),
+	deletePost: (postId: number) =>
+		({
+			type: 'profile/DELETE_POST',
+			postId,
+		} as const),
+	setUserProfile: (userProfile: ProfileType | null) =>
+		({
+			type: 'profile/SET_USER_PROFILE',
+			userProfile,
+		} as const),
+	setUserStatus: (status: string) =>
+		({
+			type: 'profile/SET_USER_STATUS',
+			status,
+		} as const),
+	updateUserStatus: (status: string) =>
+		({
+			type: 'profile/UPDATE_USER_STATUS',
+			status,
+		} as const),
+	savePhotoSuccess: (userPhoto: PhotosType) =>
+		({
+			type: 'profile/SAVE_USER_PHOTO_SUCCESS',
+			userPhoto,
+		} as const),
+	updateUserDataSuccess: (userDataStatus: string) =>
+		({
+			type: 'profile/UPDATE_USER_DATA_SUCCESS',
+			userDataStatus,
+		} as const),
 }
-export const addPost = (newPostBody: string ): AddPostType => ({
-	type: ADD_POST,
-	newPostBody,
-})
 
-type DeletePostType = {
-	type: typeof DELETE_POST
-	postId: number
-}
-export const deletePost = (postId: number): DeletePostType => ({
-	type: DELETE_POST,
-	postId,
-})
-
-type SetUserProfileType = {
-	type: typeof SET_USER_PROFILE
-	userProfile: ProfileType | null
-}
-export const setUserProfile = (userProfile: ProfileType | null): SetUserProfileType => ({
-	type: SET_USER_PROFILE,
-	userProfile,
-})
-
-type SetUserStatusType = {
-	type: typeof SET_USER_STATUS
-	status: string
-}
-export const setUserStatus = (status: string): SetUserStatusType => ({
-	type: SET_USER_STATUS,
-	status,
-})
-
-type UpdateUserStatusType = {
-	type: typeof UPDATE_USER_STATUS
-	status: string
-}
-export const updateUserStatus = (status: string): UpdateUserStatusType => ({
-	type: UPDATE_USER_STATUS,
-	status,
-})
-
-type SavePhotoSuccessType = {
-	type: typeof SAVE_USER_PHOTO_SUCCESS
-	userPhoto: PhotosType
-}
-export const savePhotoSuccess = (userPhoto: PhotosType): SavePhotoSuccessType => ({
-	type: SAVE_USER_PHOTO_SUCCESS,
-	userPhoto,
-})
-
-type UpdateUserDataSuccessType = {
-	type: typeof UPDATE_USER_DATA_SUCCESS
-	userDataStatus: string
-}
-export const updateUserDataSuccess = (userDataStatus: string): UpdateUserDataSuccessType => ({
-	type: UPDATE_USER_DATA_SUCCESS,
-	userDataStatus,
-})
+//
 
 //--------------------------------------------------------
 // вместо явной типизации ниже, используем generic BaseThunkType из react-redux-store.ts и передаем в него в качестве параметра - ActionsTypes, остальное приходит по умолчанию
-type ThunkType = BaseThunkType<ActionTypes>
+type ThunkType = BaseThunkType<ActionTypes | ReturnType<typeof stopSubmit>> // передаем так же доп action для stopSubmit и возвращаем типы по (ReturnType<typeof stopSubmit>)
+// или другой вариант: BaseThunkType<ActionsTypes | FormAction> т.е. stopSubmit в типизациях redux-form является 'FormAction
 
 // type ThunkType = ThunkAction<Promise<void>,AppStateType, unknown, ActionTypes>
 
 //thunk creators:
 export const getUserProfile =
 	(profileId: number | null): ThunkType =>
-	async (dispatch) => {
+	async dispatch => {
 		const userProfileData = await profileApi.getProfile(profileId) // вместо then() используем async await
 		// debugger
-		dispatch(setUserProfile(userProfileData))
+		dispatch(actions.setUserProfile(userProfileData))
 	}
 
 export const getStatus =
 	(profileId: number): ThunkType =>
-	async (dispatch) => {
+	async dispatch => {
 		const status = await profileApi.getStatus(profileId)
 		// debugger
-		dispatch(setUserStatus(status))
+		dispatch(actions.setUserStatus(status))
 	}
 
 export const updateStatus =
 	(status: string): ThunkType =>
-	async (dispatch) => {
+	async dispatch => {
 		//используем try catch для перехвата ошибок
 		try {
 			const updateStatusData = await profileApi.updateStatus(status)
-			updateStatusData.resultCode === ResultCodesEnum.Success && dispatch(setUserStatus(status))
+			updateStatusData.resultCode === ResultCodesEnum.Success && dispatch(actions.setUserStatus(status))
 		} catch (error) {
 			console.log(error)
 		}
@@ -181,9 +159,9 @@ export const updateStatus =
 
 export const savePhoto =
 	(file: File): ThunkType =>
-	async (dispatch) => {
+	async dispatch => {
 		const savePhotoData = await profileApi.savePhoto(file)
-		savePhotoData.resultCode === ResultCodesEnum.Success && dispatch(savePhotoSuccess(savePhotoData.data.photos))
+		savePhotoData.resultCode === ResultCodesEnum.Success && dispatch(actions.savePhotoSuccess(savePhotoData.data.photos))
 	}
 
 // чтобы получить обновленную версию profile после редактирования
@@ -191,7 +169,6 @@ export const savePhoto =
 export const saveUserData =
 	(profile: ProfileType): ThunkType =>
 	async (dispatch, getState) => {
-
 		const userId = getState().auth.id
 		const userData = await profileApi.saveUserData(profile)
 
@@ -199,10 +176,85 @@ export const saveUserData =
 			dispatch(getUserProfile(userId))
 		} else {
 			//передаем в качестве ошибки сообщение из response.data.messages из api запроса
-			// dispatch(stopSubmit('edit-profile', { _error: userData.messages }))//для выведения общей ошибки ??? types ????
+			dispatch(stopSubmit('edit-profile', { _error: userData.messages }))//для выведения общей ошибки ??? types ????
 			// dispatch(stopSubmit('edit-profile', { "contacts":  {"facebook": response.data.messages[0]} }))//для выведения отдельных ошибок по полям но надо распарсить строку из response.data.messages[0]
 			return Promise.reject(userData.messages) //после диспатча возвращаем Promise.reject с сообщением об этой ошибки внутри
 		}
 	}
 
 export default profileReducer
+
+//----- старая версия типизации без generic inferActionTypes -------------
+
+// type ActionTypes =
+// 	| AddPostType
+// 	| DeletePostType
+// 	| SetUserProfileType
+// 	| SetUserStatusType
+// 	| UpdateUserStatusType
+// 	| SavePhotoSuccessType
+// 	| UpdateUserDataSuccessType
+
+// //Action Creators with types:
+// type AddPostType = {
+// 	type: typeof ADD_POST
+// 	newPostBody: string
+// }
+// export const addPost = (newPostBody: string): AddPostType => ({
+// 	type: ADD_POST,
+// 	newPostBody,
+// })
+
+// type DeletePostType = {
+// 	type: typeof DELETE_POST
+// 	postId: number
+// }
+// export const deletePost = (postId: number): DeletePostType => ({
+// 	type: DELETE_POST,
+// 	postId,
+// })
+
+// type SetUserProfileType = {
+// 	type: typeof SET_USER_PROFILE
+// 	userProfile: ProfileType | null
+// }
+// export const setUserProfile = (userProfile: ProfileType | null): SetUserProfileType => ({
+// 	type: SET_USER_PROFILE,
+// 	userProfile,
+// })
+
+// type SetUserStatusType = {
+// 	type: typeof SET_USER_STATUS
+// 	status: string
+// }
+// export const setUserStatus = (status: string): SetUserStatusType => ({
+// 	type: SET_USER_STATUS,
+// 	status,
+// })
+
+// type UpdateUserStatusType = {
+// 	type: typeof UPDATE_USER_STATUS
+// 	status: string
+// }
+// export const updateUserStatus = (status: string): UpdateUserStatusType => ({
+// 	type: UPDATE_USER_STATUS,
+// 	status,
+// })
+
+// type SavePhotoSuccessType = {
+// 	type: typeof SAVE_USER_PHOTO_SUCCESS
+// 	userPhoto: PhotosType
+// }
+// export const savePhotoSuccess = (userPhoto: PhotosType): SavePhotoSuccessType => ({
+// 	type: SAVE_USER_PHOTO_SUCCESS,
+// 	userPhoto,
+// })
+
+// type UpdateUserDataSuccessType = {
+// 	type: typeof UPDATE_USER_DATA_SUCCESS
+// 	userDataStatus: string
+// }
+// export const updateUserDataSuccess = (userDataStatus: string): UpdateUserDataSuccessType => ({
+// 	type: UPDATE_USER_DATA_SUCCESS,
+// 	userDataStatus,
+// })
