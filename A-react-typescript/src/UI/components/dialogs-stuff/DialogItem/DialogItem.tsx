af3a2eb2fe1f1642a89eec9c2007b6f29f6060d1
crossroads30 <crossroads30@mail.ru>
2023-11-28 13:20:49 +0300

    refactor: add types, rename ex to tsx

--- A-react-typescript/src/UI/components/dialogs-stuff/DialogItem/DialogItem.tsx
+++ A-react-typescript/src/UI/components/dialogs-stuff/DialogItem/DialogItem.tsx
@@ -1,10 +1,16 @@
 import cl from './DialogItem.module.css'
 import { NavLink } from 'react-router-dom'
+import React from 'react'
 
-const DialogItem = (props) => {
-  const path = '/dialogs/' + props.id
-  
-  return (
+type PropsType = {
+	id: number
+	name: string
+}
+
+const DialogItem: React.FC<PropsType> = props => {
+	const path = '/dialogs/' + props.id
+
+	return (
 		<li className={cl.item}>
 			<NavLink to={path}>{props.name}</NavLink>
 		</li>
