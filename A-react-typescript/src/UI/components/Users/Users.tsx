901fb77cc9606985da573918b623add2964854df
crossroads30 <crossroads30@mail.ru>
2023-12-11 15:20:22 +0300

    feat: add formik

--- A-react-typescript/src/UI/components/Users/Users.tsx
+++ A-react-typescript/src/UI/components/Users/Users.tsx
@@ -5,6 +5,7 @@ import Paginator from '../common/Paginator/PagePaginator.tsx'
 import SeparateUser from './SeparateUser.tsx'
 import React, { FC } from 'react'
 import { UserType } from '../../../types/types.ts'
+import { Field, Form, Formik } from 'formik'
 
 type PropsType = {
 	totalUsersCount: number
@@ -19,10 +20,11 @@ type PropsType = {
 	setFriend: () => void
 }
 
-const Users: FC<PropsType> = ({totalUsersCount, pageSize, onPageChange, currentPage, ...props}) => { 
+const Users: FC<PropsType> = ({ totalUsersCount, pageSize, onPageChange, currentPage, ...props }) => {
 	return (
 		<>
 			<div className={cl.usersWrapper}>
+				<UserSearchForm/>
 				<Paginator totalItemsCount={totalUsersCount} pageSize={pageSize} onPageChange={onPageChange} currentPage={currentPage} />
 				{props.isLoading ? (
 					<Preloader />
@@ -40,6 +42,52 @@ const Users: FC<PropsType> = ({totalUsersCount, pageSize, onPageChange, currentP
 				)}
 			</div>
 		</>
+	)
+}
+
+type usersSearchFormObjectType = {
+	term: string
+}
+
+const usersSearchFormValidates = (values: any) => {
+	const errors = {}
+	return errors
+}
+
+
+const UserSearchForm: FC = () => {
+
+	const submit = (values: usersSearchFormObjectType, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
+		// setTimeout(() => {
+		// 	// alert(JSON.stringify(values, null, 2))
+		// 	setSubmitting(false)
+		// }, 400)
+		
+	}
+
+
+	return (
+		<div>
+			<Formik initialValues={{ term: '' }} validate={usersSearchFormValidates} onSubmit={submit}>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					isSubmitting,
+					/* and other goodies */
+				}) => (
+					<Form onSubmit={handleSubmit}>
+						<Field type='text' name='term' />
+						<button type='submit' disabled={isSubmitting}>
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
+		</div>
 	)
 }
 
